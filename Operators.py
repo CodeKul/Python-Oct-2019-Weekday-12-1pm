197debe678ff9fa6574f9f9ec0d100fd1a8ac1bc
Varun Kudalkar <varun.kudalkar@gmail.com>
2019-10-17 13:44:53 +0530

    operators demo

---
+++ Operators.py
@@ -0,0 +1,73 @@
+# Assignment
+'''
+    =
+'''
+
+a = 10
+b = 20
+
+
+# Arithmetic
+'''
+    +
+    -
+    *
+    /
+    %
+    **
+'''
+
+res = a ** b
+
+# Compound Assignment
+'''
+    +=
+    -=
+    /=
+    *=
+    %=
+    **=
+'''
+
+a += b # a = a + b
+
+# Comparision
+'''
+   >
+   <
+   <=
+   >=
+   ==
+   != 
+'''
+
+res = a != b
+
+# Logical
+'''
+    and
+    or
+    not
+
+    p   q   and or  not(p)
+    0   0   0   0   1
+    0   1   0   1   1
+    1   0   0   1   0
+    1   1   1   1   0
+'''
+
+res = not((a < b) and (a < 50))
+
+c = True
+
+d = res + c
+
+print(d)
+
+'''
+1) git clone
+2) git add
+3) git commit
+4) git push
+5) git pull
+'''
